b05d60892b1c305a4f69d7fd13c7d1d39d7b12be
Örjan Sjöholm <orjansjoholm@gmail.com>
2012-11-02 02:12:19 +0100

    Added event emitter

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 var teamcity = require('./lib/teamcity');
 var raspberry = require('./lib/raspberry');
+var emitter = require('events').EventEmitter;
+var events = new emitter();
 
 var tc = teamcity.create("http://176.34.231.33", "orjan", "SeaBassNinja");
 
 var checkBuildStatus = function() {
 	tc.hasFailingProjects().on('complete', function(buildStatus) {
 		var hasFailingProjects = buildStatus.count > 0;
-		if (hasFailingProjects) {
-			console.log("fail");
-		}
-		else {
-			console.log("ok");
-		}
+		events.emit("updateBuildStatus", hasFailingProjects);
 	});
 };
 
-setInterval(checkBuildStatus, 1000);
+setInterval(function() {
+	events.emit("checkBuildStatus");
+}, 1000);
+
+events.on("checkBuildStatus", checkBuildStatus);
+
+events.on("updateBuildStatus", function(hasFailingProjects) {
+	console.log("New status: ", hasFailingProjects);
+});
+
+events.on("updateBuildStatus", function(hasFailingProjects) {
+	if (hasFailingProjects) {
+		raspberry.on(4);
+	}
+	else {
+		raspberry.off(4);
+	}
+});
